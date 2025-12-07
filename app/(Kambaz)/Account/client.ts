6fd6792a28485f1884855bcccb6846b32921509e
Cole <coleabrams11@gmail.com>
2025-12-07 17:29:37 -0500

    okay

--- app/(Kambaz)/Account/client.ts
+++ app/(Kambaz)/Account/client.ts
@@ -56,7 +56,7 @@ export const createUser = async (user: any) => {
 };
 
 export const profile = async () => {
-  const response = await axiosWithCredentials.post(`${USERS_API}/profile`);
+  const response = await axiosWithCredentials.get(`${USERS_API}/profile`);
   return response.data;
 };
 
