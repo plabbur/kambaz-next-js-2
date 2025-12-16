07b5c3892493bc7e7a96b57ae50e663b285b09ac
Cole <coleabrams11@gmail.com>
2025-12-15 19:00:19 -0500

    lets try this

--- app/(Kambaz)/Courses/[cid]/Assignments/client.ts
+++ app/(Kambaz)/Courses/[cid]/Assignments/client.ts
@@ -1,24 +1,35 @@
 import axios from "axios";
+const axiosWithCredentials = axios.create({ withCredentials: true });
 
 const BASE = process.env.NEXT_PUBLIC_API_BASE || "http://localhost:4000";
 
 export const findAssignmentsForCourse = async (courseId: string) => {
-  const res = await axios.get(`${BASE}/api/courses/${courseId}/assignments`);
+  const res = await axiosWithCredentials.get(
+    `${BASE}/api/courses/${courseId}/assignments`
+  );
   return res.data;
 };
 
 export const createAssignment = async (assignment: any) => {
-  const res = await axios.post(`${BASE}/api/assignments`, assignment);
+  const res = await axiosWithCredentials.post(
+    `${BASE}/api/assignments`,
+    assignment
+  );
   return res.data;
 };
 
 export const deleteAssignment = async (aid: string) => {
-  const res = await axios.delete(`${BASE}/api/assignments/${aid}`);
+  const res = await axiosWithCredentials.delete(
+    `${BASE}/api/assignments/${aid}`
+  );
   return res.status === 200;
 };
 
 export const updateAssignment = async (assignment: any) => {
-  const res = await axios.put(`${BASE}/api/assignments/${assignment._id}`, assignment);
+  const res = await axiosWithCredentials.put(
+    `${BASE}/api/assignments/${assignment._id}`,
+    assignment
+  );
   return res.data;
 };
 
